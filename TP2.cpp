808e5f4b8ebd1d0172f87d7eea9d9accd9fcc7e5
JulianBarrios <julianobarrios.jb@gmail.com>
2015-04-23 01:48:13 -0300

    Tp2-numero par o impar

---
+++ TP2.cpp
@@ -0,0 +1,24 @@
+/*
+*Numero par o impar
+*Julian Barrios
+*22-04-2015
+*/
+
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"ingrese un numero: ";
+	cin>>num;
+	if(num%2==0)
+	{
+		cout<<"es par";
+	}
+	else
+	{
+		cout<<"es impar";
+	}
+	return 0;
+}
